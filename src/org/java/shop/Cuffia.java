e6c2572347d5a255b317f8ab9c49d2504f992c99
mcspe <mcspezzaferro@gmail.com>
2023-09-18 16:23:42 +0200

    [add]classe Cuffia

---
+++ src/org/java/shop/Cuffia.java
@@ -0,0 +1,48 @@
+package org.java.shop;
+
+public class Cuffia extends Prodotto {
+	
+	private String colore;
+	private boolean wireless;
+	
+	public Cuffia(String nome, String marca, double prezzo, int iva, String colore, boolean wireless) {
+		super(nome, marca, prezzo, iva);
+		setColore(colore);
+		setWireless(wireless);
+	}
+	
+	public void setColore(String colore) {
+		this.colore = colore;
+	}
+	
+	public String getColore() {
+		return colore;
+	}
+	
+	public void setWireless(boolean wireless) {
+		this.wireless = wireless;
+	}
+	
+	public boolean isWireless() {
+		return wireless;
+	}
+	
+	@Override
+	protected String getCat() {
+		return "Cuffie";
+	}
+	
+	@Override
+	public String toString() {
+		return "Categoria: " + getCat() + 
+				"\\nMarca: " + getMarca() + 
+				"\\nNome: " + getNome() + 
+				"\\nCodice Prodotto: " + getCodice() + 
+				"\\nColore: " + getColore() + "\"" +
+				(isWireless() ? "\\nWireless" : "\\nCablate") +
+				"\\nPrezzo base: " + getPrezzoBase() + "€" +
+				"\\nIva: " + getIva() + "%" + 
+				"\\nPrezzo :" + getPrezzoConIva() + "€";
+	}
+
+}
